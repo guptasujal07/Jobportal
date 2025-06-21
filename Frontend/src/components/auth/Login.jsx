1a4577b047837248e5e0e29189ca5da2a54b40eb
guptasujal07 <guptasujal3020@gmail.com>
2025-06-22 01:05:08 +0530

    data complete save

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -1,27 +1,52 @@
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
-import { RadioGroup,} from '../ui/radio-group'
+import { RadioGroup, } from '../ui/radio-group'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 const Login = () => {
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+        role: ""
+    });
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+    }
+
+
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
     return (
         <div>
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Login</h1>
-                    
+
                     <div className='my-2'>
                         <Label>Email</Label>
-                        <Input type="email" placeholder="sujal@gmail.com" />
+                        <Input type="email"
+                            value={input.email}
+                            name="email"
+                            onChange={changeEventHandler}
+                            placeholder="sujal@gmail.com" />
                     </div>
-                   
+
                     <div className='my-2'>
                         <Label>Password</Label>
-                        <Input type="password" placeholder="" />
+                        <Input type="password"
+                            value={input.password}
+                            name="password"
+                            onChange={changeEventHandler}
+                            placeholder="" />
                     </div>
                     <div className='flex items-center justify-between'>
                         <RadioGroup className='flex items-center gap-4 my-5'>
@@ -30,6 +55,8 @@ const Login = () => {
                                     type="radio"
                                     name="role"
                                     value="student"
+                                    checked={input.role == 'student'}
+                                    onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
                                 <Label htmlFor="r1">Student</Label>
--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,33 +1,75 @@
 import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
-import { RadioGroup,} from '../ui/radio-group'
+import { RadioGroup, } from '../ui/radio-group'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 const Signup = () => {
+
+    const [input, setInput] = useState({
+        fullname: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+        role: "",
+        file: ""
+    });
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+    }
+
+    const changeFileHandler = (e) => {
+        setInput({ ...input, file: e.target.files?.[0] });
+    }
+
+
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
+
     return (
         <div>
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
-                <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+                <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
                     <h1 className='font-bold text-xl mb-5'>Sign Up</h1>
                     <div className='my-2'>
                         <Label>Full Name</Label>
-                        <Input type="text" placeholder="sujal" />
+                        <Input type="text"
+                            value={input.fullname}
+                            name="fullname"
+                            onChange={changeEventHandler}
+                            placeholder="sujal" />
                     </div>
                     <div className='my-2'>
                         <Label>Email</Label>
-                        <Input type="email" placeholder="sujal@gmail.com" />
+                        <Input type="email"
+                            value={input.email}
+                            name="email"
+                            onChange={changeEventHandler}
+                            placeholder="sujal@gmail.com" />
                     </div>
                     <div className='my-2'>
                         <Label>Phone number</Label>
-                        <Input type="phonenumber" placeholder="9896523655" />
+                        <Input type="phonenumber"
+                            value={input.phoneNumber}
+                            name="phoneNumber"
+                            onChange={changeEventHandler}
+                            placeholder="9896523655" />
                     </div>
                     <div className='my-2'>
                         <Label>Password</Label>
-                        <Input type="password" placeholder="" />
+                        <Input type="password"
+                            value={input.password}
+                            name="password"
+                            onChange={changeEventHandler}
+                            placeholder="" />
                     </div>
                     <div className='flex items-center justify-between'>
                         <RadioGroup className='flex items-center gap-4 my-5'>
@@ -36,6 +78,8 @@ const Signup = () => {
                                     type="radio"
                                     name="role"
                                     value="student"
+                                    checked={input.role == 'student'}
+                                    onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
                                 <Label htmlFor="r1">Student</Label>
@@ -45,6 +89,8 @@ const Signup = () => {
                                     type="radio"
                                     name="role"
                                     value="recruiter"
+                                    checked={input.role == 'recruiter'}
+                                    onChange={changeEventHandler}
                                     className="cursor-pointer"
                                 />
                                 <Label htmlFor="r2">Recruiter</Label>
@@ -56,6 +102,7 @@ const Signup = () => {
                             <Input
                                 accept="image/*"
                                 type="file"
+                                onChange={changeFileHandler}
                                 className='cursor-pointer'
                             />
                         </div>
