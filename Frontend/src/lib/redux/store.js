10695fd930eb192b188847f9a9a097f41fcfaf94
guptasujal07 <guptasujal3020@gmail.com>
2025-07-11 12:53:22 +0530

    commit

---
+++ Frontend/src/lib/redux/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storageSession from "redux-persist/storage/session";
+
+
+const persistConfig = {
+    key: "auth",           
+    storage: storageSession,
+    version: 1,
+};
+
+
+const persistedAuthReducer = persistReducer(persistConfig, authSlice);
+
+
+const store = configureStore({
+    reducer: {
+        auth: persistedAuthReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false 
+        }),
+});
+
+
+export const persistor = persistStore(store);
+
+export default store;
