206d39dae0e727a7f728058766ec2e6883378f97
guptasujal07 <guptasujal3020@gmail.com>
2025-08-02 23:22:02 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,10 +4,13 @@ import Navbar from './components/shared/Navbar';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Home from './components/Home';
-import Jobs from './components/Jobs'; 
+import Jobs from './components/Jobs';
 import Browse from './components/Browse'
 import Profile from './components/Profile';
 import JobDescription from './components/JobDescription'
+import Companies from './components/admin/Companies';
+import CompanyCreate from './components/admin/CompanyCreate';
+import CompanySetup from './components/admin/CompanySetup';
 
 const appRouter = createBrowserRouter([
   {
@@ -34,10 +37,25 @@ const appRouter = createBrowserRouter([
     path: "/profile",
     element: <Profile />
   },
-   {
+  {
     path: "/jobs/description/:id",
     element: <JobDescription />
+  },
+  // admin ke liye yha se start hoga
+
+  {
+    path: "/admin/companies",
+    element: <Companies />
+  },
+   {
+    path: "/admin/companies/create",
+    element: <CompanyCreate />
+  },
+    {
+    path:"/admin/companies/:id",
+    element:<CompanySetup/>
   }
+
 ]);
 
 function App() {
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,34 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-import jobReducer from './jobSlice'; 
+import jobReducer from "./jobSlice";
+import companySlice from "./companySlice"; // ✅ MISSING IMPORT
 
 import { persistReducer, persistStore } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
 
-
 const persistConfig = {
-    key: "auth",           
-    storage: storageSession,
-    version: 1,
+  key: "auth",
+  storage: storageSession,
+  version: 1,
 };
-
 
 const persistedAuthReducer = persistReducer(persistConfig, authSlice);
 
-
-
 const store = configureStore({
-    reducer: {
-        auth: persistedAuthReducer,
-         job: jobReducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false 
-        }),
+  reducer: {
+    auth: persistedAuthReducer,
+    job: jobReducer,
+    company: companySlice, // ✅ Now valid
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-
 export const persistor = persistStore(store);
-
 export default store;
